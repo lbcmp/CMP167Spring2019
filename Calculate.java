7aac97d81b0c2f6689b252916cf5faaf0221ab68
lbcmp <47428116+lbcmp@users.noreply.github.com>
2019-04-09 12:41:42 -0400

    Add files via upload

--- Calculate.java
+++ Calculate.java
@@ -1,22 +1,53 @@
-/*
- * Author: Leah
- * Created: 4/2/19
- * Desc: This program has calculate methods
- */
-
 public class Calculate {
 
-	public static int multiply(int a, int b) {
-		return a * b;
+	public static int sumListMethod(int[] array) {
+		int sum = 0;
+		for (int i = 0; i < array.length; ++i) {
+			sum += array[i];
+		}
+		return sum;
 	}
 	
-	public static double multiply(double a, double b) {
-		return a * b;
-	}
-	public static double getRectArea(double width, double length) {
-		return width * length;
+	public static int avgListMethod(int[] array) {
+		int avg = sumListMethod(array) / array.length;
+		return avg;
 	}
 	
+	public static int oddElements(int[] array) {
+		int numOdd = 0;
+		for (int i = 0; i < array.length; ++i) {
+			if (array[i] % 2 != 0) {
+				++numOdd;
+			}
+		}
+		return numOdd;
+	}
 	
-	
+	public static void main(String[] args) {
+		/* int[] array = {3, 4, 5, 8};
+		System.out.println("Sum is " + sumListMethod(array));
+		System.out.println("Avg is " + avgListMethod(array));
+		System.out.println("The number of odd elements is " + oddElements(array));
+		*/
+		
+		int[] array = {121, 4, 1000};
+	      int max = array[0];
+	      int min = array[0];
+	      int median = array[0];
+	      
+	      for (int i = 0; i < array.length; ++i) {
+	         if (array[i] > max) {
+	          max = array[i];  
+	         }
+	         else if (array[i] < min) {
+	          min = array[i];  
+	         }
+	      }
+	      for (int i = 0; i < array.length; ++i) {
+	         if ((array[i] < max) && (array[i] > min)) {
+	            median = array[i];
+	         }
+	      }
+	      System.out.println(median);
+	}
 }
